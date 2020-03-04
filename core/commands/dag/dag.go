f6fd876a706562a6c0ee1681a2ff2a521971d061
Peter Rabbitson <ribasushi@protocol.ai>
2020-03-04 16:11:14 +0100

    Initial version for review

--- core/commands/dag/dag.go
+++ core/commands/dag/dag.go
@@ -1,6 +1,7 @@
 package dagcmd
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"math"
@@ -9,6 +10,7 @@ import (
 	"github.com/ipfs/go-ipfs/core/commands/cmdenv"
 	"github.com/ipfs/go-ipfs/core/coredag"
 
+	gocar "github.com/ipfs/go-car"
 	cid "github.com/ipfs/go-cid"
 	cidenc "github.com/ipfs/go-cidutil/cidenc"
 	cmds "github.com/ipfs/go-ipfs-cmds"
@@ -17,6 +19,13 @@ import (
 	ipfspath "github.com/ipfs/go-path"
 	path "github.com/ipfs/interface-go-ipfs-core/path"
 	mh "github.com/multiformats/go-multihash"
+)
+
+const (
+	//progressOptionName = "progress"
+	//quietOptionName    = "quiet"
+	silentOptionName   = "silent"
+	pinRootsOptionName = "pin-roots"
 )
 
 var DagCmd = &cmds.Command{
@@ -33,10 +42,11 @@ to deprecate and replace the existing 'ipfs object' command moving forward.
 		"put":     DagPutCmd,
 		"get":     DagGetCmd,
 		"resolve": DagResolveCmd,
+		"import":  DagImportCmd,
 	},
 }
 
-// OutputObject is the output type of 'dag put' command
+// OutputObject is the output type of 'dag put'/'dag import'command
 type OutputObject struct {
 	Cid cid.Cid
 }
@@ -240,4 +250,196 @@ var DagResolveCmd = &cmds.Command{
 		}),
 	},
 	Type: ResolveOutput{},
+}
+
+var DagImportCmd = &cmds.Command{
+	Helptext: cmds.HelpText{
+		Tagline: "Import the contents of .car files",
+		ShortDescription: `
+'ipfs dag import' parses .car files and adds all contained objects to the blockstore.
+`,
+	},
+
+	Arguments: []cmds.Argument{
+		// Q: what is the EnableRecursive for in the context of paths ?
+		cmds.FileArg("path", true, true, "The path of a .car file.").EnableStdin(),
+	},
+	Options: []cmds.Option{
+		// FIXME - need to copy the progress bar from core/commands/add.go
+		// Q: Is the progress bar implementation "legit"? Also is "bar per car" ok?
+		//cmds.BoolOption(progressOptionName, "p", "Stream progress data.").WithDefault(true),
+		//cmds.BoolOption(quietOptionName, "q", "Write minimal output."),
+		cmds.BoolOption(silentOptionName, "Write no output."),
+		cmds.BoolOption(pinRootsOptionName, "Pin roots optionally listed in the .car header when adding.").WithDefault(true),
+	},
+	// FIXME - progress bar option disabled above
+	// PreRun: func(req *cmds.Request, env cmds.Environment) error {
+	// 	quiet, _ := req.Options[quietOptionName].(bool)
+	// 	silent, _ := req.Options[silentOptionName].(bool)
+
+	// 	if quiet || silent {
+	// 		return nil
+	// 	}
+
+	// 	// ipfs cli progress bar defaults to true unless quiet or silent is used
+	// 	_, found := req.Options[progressOptionName].(bool)
+	// 	if !found {
+	// 		req.Options[progressOptionName] = true
+	// 	}
+
+	// 	return nil
+	// },
+	Type: OutputObject{},
+	Run: func(req *cmds.Request, res cmds.ResponseEmitter, env cmds.Environment) error {
+		api, err := cmdenv.GetApi(env, req)
+		if err != nil {
+			return err
+		}
+
+		doPinRoots, _ := req.Options[pinRootsOptionName].(bool)
+		silent, _ := req.Options[silentOptionName].(bool)
+
+		keepTrackOfRoots := doPinRoots || !silent
+
+		// It is not guaranteed that a root in a header is actually present in the same ( or any .car )
+		// Accumulate any root CID seen in a header, and supplement its actual node if/when encountered
+		// We will pin *only* at the end in case the entire operation is successful
+		// Q: - way to do above better?
+		var expectRoots map[cid.Cid]*ipld.Node
+
+		if keepTrackOfRoots {
+			expectRoots = make(map[cid.Cid]*ipld.Node, 32)
+		}
+
+		// If more than one file - parse all headers first
+		// ( due to misdesign we do "reparse" each car's header twice, but that's cheap )
+		// Q: there seems to be no management of file closures...
+		argCount, _ := req.Files.Size()
+		if keepTrackOfRoots && argCount > 1 {
+
+			it := req.Files.Entries()
+			for it.Next() {
+
+				file := files.FileFromEntry(it)
+				if file == nil {
+					return errors.New("expected a file")
+				}
+
+				car, err := gocar.NewCarReader(file)
+				if err != nil {
+					return err
+				}
+
+				for _, cid := range car.Header.Roots {
+					if _, exists := expectRoots[cid]; !exists {
+						expectRoots[cid] = nil
+					}
+				}
+
+				// rewind - we will come back
+				file.Seek(0, 0)
+			}
+		}
+
+		adder := api.Dag()
+		b := ipld.NewBatch(req.Context, adder)
+
+		it := req.Files.Entries()
+		for it.Next() {
+
+			file := files.FileFromEntry(it)
+			if file == nil {
+				return errors.New("expected a file")
+			}
+			defer func() { file.Close() }()
+
+			car, err := gocar.NewCarReader(file)
+			if err != nil {
+				return err
+			}
+
+			if car.Header.Version != 1 {
+				return errors.New("only car files version 1 supported at present")
+			}
+
+			// duplicate of a block from above: in case we were streaming and never pre-read the file
+			if keepTrackOfRoots {
+				for _, cid := range car.Header.Roots {
+					if _, exists := expectRoots[cid]; !exists {
+						expectRoots[cid] = nil
+					}
+				}
+			}
+
+			for {
+				block, err := car.Next()
+				if err != nil && err != io.EOF {
+					return err
+				} else if block == nil {
+					break
+				}
+
+				nd, err := ipld.Decode(block)
+				if err != nil {
+					return err
+				}
+
+				if err := b.Add(req.Context, nd); err != nil {
+					return err
+				}
+
+				if keepTrackOfRoots {
+					val, exists := expectRoots[nd.Cid()]
+
+					// encountered something known to be a root, for the first time
+					if exists && val == nil {
+						expectRoots[nd.Cid()] = &nd
+						if !silent {
+							if err := res.Emit(&OutputObject{Cid: nd.Cid()}); err != nil {
+								return err
+							}
+						}
+					}
+				}
+			}
+		}
+
+		if err := it.Err(); err != nil {
+			return err
+		}
+
+		if doPinRoots && len(expectRoots) > 0 {
+			// run through everything making sure we found all roots
+			toPin := make([]ipld.Node, 0, len(expectRoots))
+			for cid, nd := range expectRoots {
+				if nd == nil {
+					return fmt.Errorf("the CID '%s' named as root, but never encountered in any .car", cid)
+				}
+				toPin = append(toPin, *nd)
+			}
+
+			// all found - pin them down
+			// ( this does in effect call b.Commit()... it seems )
+			if err := api.Dag().Pinning().AddMany(req.Context, toPin); err != nil {
+				return err
+			}
+		}
+
+		// well... here goes nothing
+		if err := b.Commit(); err != nil {
+			return err
+		}
+
+		return nil
+	},
+	Encoders: cmds.EncoderMap{
+		cmds.Text: cmds.MakeTypedEncoder(func(req *cmds.Request, w io.Writer, out *OutputObject) error {
+			enc, err := cmdenv.GetLowLevelCidEncoder(req)
+			if err != nil {
+				return err
+			}
+			fmt.Fprintln(w, enc.Encode(out.Cid))
+			return nil
+		}),
+	},
 }
